894becba31af151103a91676dc60520dc35b5844
hyzic <isaac.oghenevwapo@gmail.com>
2025-01-16 14:10:08 -0500

    Configured Endpoints for Endpoints on the AppSettings

---
+++ ReadyPointer.API/Config/EndpointsConfig.cs
@@ -0,0 +1,7 @@
+namespace ReadyPointer.API.Config;
+
+public class EndpointsConfig
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public string GetAllObjectsUrl { get; set; } = string.Empty;
+}
--- ReadyPointer.API/Program.cs
+++ ReadyPointer.API/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.OpenApi.Models;
+using ReadyPointer.API.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<EndpointsConfig>(builder.Configuration.GetSection("EndpointsConfig"));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ReadyPointer Restful API",
+        Version = "v1",
+        Description = "An example ASP.NET Core Web API to perform all API operations.",
+    });
+});
 
 var app = builder.Build();
 
